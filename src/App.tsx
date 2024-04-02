5b6c757a1e4c832b40b04126412f183bf9055c09
Pravanjan <pravanjan@Apples-Laptop.local>
2024-04-02 10:53:22 +0530

    husky configuration with pre-commit hooks testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,30 @@
 import { useState } from 'react';
 import './App.css';
 
-
-
 const App = () => {
+    return (
+        <div>
+            <InputComponent />
+        </div>
+    );
 
-  return (
-    <div>
-      <InputComponent/>
-      <Childcompnent/>
-    </div>
-  );
+    function InputComponent() {
+        const [input, setInput] = useState('');
 
-  function InputComponent (){
-    const [input, setInput] = useState("");
+        return <input type='text' value={input} onChange={setValue} />;
+        function setValue(e: any) {
+            console.log('I am just a demo function ');
 
-     return (
-      <input type="text" value ={input} onChange={setValue}/>
-
-     )
-     function setValue (e:any){
-      console.log("I am just a demo function ");
-  
-      setInput(e.target.value);
+            setInput(e.target.value);
+        }
     }
-
-  }
- 
 };
 
-function Childcompnent(){
-  return (
-    <div>
-      <span>I am inside child component</span>
-    </div>
-  )
+function Childcompnent() {
+    return (
+        <div>
+            <span>I am inside child component</span>
+        </div>
+    );
 }
 export default App;
